f0ce30fe8866d96242b83cfbdde8cf2cfe4d0eca
joandso <95076396+joandso@users.noreply.github.com>
2022-06-08 16:20:10 +0100

    Update joandso-map.js

--- joandso-map.js
+++ joandso-map.js
@@ -187,7 +187,7 @@ $(".region-items").on("click", function () {
   map.flyTo({
     center: regionLocations.features[myIndex].geometry.coordinates,
     speed: 2,
-    zoom: 13,
+    zoom: 7,
     curve: 1,
     easing(t) {
       return t;
